62a47b1a2cbf7f7a5f4f86cc0205e840da6f3224
Dexter Nunag <nunagdexter@gmail.com>
2021-08-05 23:56:07 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+
+
+function App() {
+  return (
+    <AppContainer>
+      Hello World
+    </AppContainer>
+  );
+}
+
+export default App;
---
+++ src/app/services/animeService/queries.tsx
@@ -0,0 +1,24 @@
+import { gql } from '@apollo/client'
+
+export const GET_ANIME_PAGE = gql`
+    query GetAnimePage(
+        $page: Int!,
+        $perPage: Int!
+    ) {
+        Page(
+            page: $page,
+            perPage: $perPage
+        ) {
+            media {
+                id
+                description
+                title {
+                    english
+                }
+                coverImage {
+                    medium
+                }
+            }
+        }
+    }
+`
